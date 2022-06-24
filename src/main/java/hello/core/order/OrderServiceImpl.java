6b85b9cf2b405d32b23c4672073fc27635156aa6
KDH_Notebook <destiny1017@nate.com>
2022-06-24 17:13:16 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -22,4 +22,9 @@ public class OrderServiceImpl implements OrderService {
         int discountedPrice = discountPolicy.discount(member, itemPrice);
         return new Order(memberId, itemName, itemPrice, discountedPrice);
     }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,37 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        statefulService1.order("userA", 10000);
+        statefulService2.order("userB", 20000);
+
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        // userB와 userA가 같은 객체를 공유하는 상태에서 값이 수정되어 userA의 주문 가격이 달라져버림
+        assertThat(price).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
